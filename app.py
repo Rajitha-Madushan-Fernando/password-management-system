4c15f56beec3dcf21c289a58ff2ea02348c414e2
Rajitha Madushan <buddikarajitha@yahoo.com>
2020-12-04 01:41:50 +0100

    Implement user register module

--- app.py
+++ app.py
@@ -4,12 +4,17 @@ from flask_marshmallow import Marshmallow
 import jsonpickle
 #Exception lib 
 from werkzeug import exceptions
+import json
 import os
-from datetime import date
+import jwt
+import datetime
+from functools import wraps
 
 #Import user defined libs
 from password_module.password import Password
 from db_models.password import PasswordList
+from db_models.password import LegacyApp
+from db_models.password import UserList
 
 #init app
 app = Flask(__name__)
@@ -19,8 +24,64 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 #Init ma
 ma = Marshmallow(app)
 
+#Access controll module
+#Without having a proper JWWT authentication token cannot access to API
+app.config['SECRET_KEY'] = 'rajithasecretkey'
+def token_required(f):
+    @wraps(f)
+    def wrapper(*args, **kwargs):
+        token = request.args.get('token')
+        try:
+            jwt.decode(token, app.config['SECRET_KEY'])
+            return f(*args, **kwargs)
+        except:
+            return jsonify({'error': 'Need a valid token to view this page'}), 401
+    return wrapper
+#Access controll module finished
 
-#Password module
+
+##User registration module
+@app.route('/signin', methods=['POST'])
+def register():
+
+    try:
+        request_data = request.get_json()
+        username = str(request_data['username'])
+        password = str(request_data['password'])
+        email = request_data['email']
+        role = request_data['role']
+        
+        hibp_result = Password.check_hibp(password)
+        is_complexity, complexity_result_msg  = Password.check_complexity(password)
+        hash_result = Password.hash_pwd(password)
+
+        if is_complexity is False:
+            return jsonify(Process='ERROR!', Process_Message=complexity_result_msg)
+    
+        elif hibp_result is True:
+            return jsonify(Process='ERROR!', Process_Message='This password is already in HIBP Database.')
+        
+        else:
+            #return jsonify(Process='SUCESS!', Process_Message='Good Password!')
+            #return jsonify(hash_result)
+            response = UserList.add_new_user(username,hash_result,email,role)
+            return jsonify({"Message": "Succesfuly saved"}), 201
+
+
+    except (KeyError, exceptions.BadRequest):
+        return jsonify(Process='ERROR!', Process_Message='Missing information, wrong keys or invalid JSON.')    
+    
+@app.route('/all_users', methods=['GET'])
+def get_users():
+    '''Function to get all the password in the database'''
+    response = UserList.get_all_users()
+    result = jsonpickle.encode(response)
+    return result
+##User registration module finished
+
+
+
+#Password module start
 @app.route('/add_pwd', methods=['POST'])
 def check_pwd():
     try:
@@ -57,10 +118,37 @@ def check_pwd():
 @app.route('/pwd_list', methods=['GET'])
 def get_pwd():
     '''Function to get all the password in the database'''
-    print("0000000")
     response = PasswordList.get_all_password()
     result = jsonpickle.encode(response)
     return result
+#Password module finished
+
+
+
+
+##Legacy Application module
+@app.route('/add_new_app', methods=['POST'])
+def add_legacy_app():
+    try:
+        req_data = request.get_json()
+        app_name = req_data['app_name']
+        #print(user_password)
+
+        response = LegacyApp.add_new_legacy_app(app_name)
+        return jsonify({"Message": "Succesfuly saved"}), 201
+
+
+    except (KeyError, exceptions.BadRequest):
+        return jsonify(Process='ERROR!', Process_Message='Missing information, wrong keys or invalid JSON.')
+
+@app.route('/app_list', methods=['GET'])
+def get_legacy_app():
+    '''Function to get all the app list in the database'''
+    response = LegacyApp.get_all_legacy_app()
+    result = jsonpickle.encode(response)
+    return result
+##Legacy Application module finished
+
 
 #Run server
 if __name__ == '__main__':
--- db_models/password.py
+++ db_models/password.py
@@ -4,6 +4,12 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
 import os
 from datetime import datetime
+from sqlalchemy import create_engine
+from sqlalchemy import Column, Integer, ForeignKey
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker, relationship
+
+
 
 # Initializing our database
 db = SQLAlchemy(app)
@@ -13,10 +19,9 @@ class PasswordList(db.Model):
     __tablename__ = 'tbl_app_password_list'
     id = db.Column(db.Integer, primary_key=True)
     password = db.Column(db.String(128))
-    app_id = db.Column(db.String(11))
     user_id = db.Column(db.String(11))
-    
-    
+    app_id = db.Column(db.Integer(), db.ForeignKey('tbl_legacy_application_list.id'))
+
     def json(self):
         return {
             'id': self.id,
@@ -36,4 +41,63 @@ class PasswordList(db.Model):
     def get_all_password():
         #function to get all pwd in our database to related particular user
         return [PasswordList.json(passwordList) for passwordList in PasswordList.query.all()]
-        
+
+
+# the class legacy app will inherit the db.Model of SQLAlchemy
+class LegacyApp(db.Model):
+    __tablename__ = 'tbl_legacy_application_list'
+    id = db.Column(db.Integer, primary_key=True)
+    app_name = db.Column(db.String(128))
+    
+
+
+    def json(self):
+        return {
+            'id': self.id,
+            'name': self.app_name
+        }
+
+    def add_new_legacy_app(_app_name):
+        # creating an instance of our password constructor
+        new_legacy_app = LegacyApp(app_name=_app_name)
+        db.session.add(new_legacy_app)  # add new password to database session
+        db.session.commit()  # commit changes to session
+        return new_legacy_app
+
+    def get_all_legacy_app():
+        #function to get all pwd in our database to related particular user
+        return [LegacyApp.json(legacyApp) for legacyApp in LegacyApp.query.all()]
+
+
+# the class User app will inherit the db.Model of SQLAlchemy
+class UserList(db.Model):
+    __tablename__ = 'tbl_users'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(32))
+    password = db.Column(db.String(128))
+    email = db.Column(db.String(128))
+    role = db.Column(db.String(20))
+    
+    
+
+    def json(self):
+        return {
+            'id': self.id,
+            'username': self.username,
+            'password': self.password,
+            'email': self.email,
+            'role': self.role
+        }
+
+    def add_new_user(_username,_password,_email,_role):
+        # creating an instance of our password constructor
+        new_user = UserList(role=_role,username=_username,password=_password,email=_email)
+        db.session.add(new_user)  # add new password to database session
+        db.session.commit()  # commit changes to session
+        return new_user
+    
+    def get_all_users():
+        #function to get all pwd in our database to related particular user
+        return [UserList.json(userApp) for userApp in UserList.query.all()]
+
+   

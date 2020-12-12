7849c6ff6161216fe917176dc2c7c598d34dbb0c
Rajitha Madushan <buddikarajitha@yahoo.com>
2020-12-12 19:29:43 +0100

    Implement test complexity unit test case

---
+++ password_module/test_password.py
@@ -0,0 +1,29 @@
+import unittest
+from .password import Password as pwd
+
+
+class PasswordTest(unittest.TestCase):
+
+    def setUp(self):
+        self.password1 = 'password'
+        self.password2 = '12345678'
+        self.password3 = 'ABC123456'
+        self.password4 = '!@#$AD'
+        self.password5 = '34D*&%Wgsju!'
+        self.password6 = '34D*&%Wgsju!34D*&%Wgsju!34D*&%Wgsju!'
+
+    def test_complexity(self):
+        self.assertEqual(pwd.check_complexity(self.password1), (False, 'Make sure your password contain one number'),'Make sure your password contain one number')
+        self.assertEqual(pwd.check_complexity(self.password2), (False, 'Make sure your password contain one uppercase letter'),  'Failed Password Test : Make sure your password contain one uppercase letter')
+        self.assertEqual(pwd.check_complexity(self.password3), (False, 'Make sure your password contain one lowercase letter'),  'Failed Password Test : Make sure your password contain one lowercase letter')
+        self.assertEqual(pwd.check_complexity(self.password4), (False, 'Make sure your password is at least 8 letters'),  'Failed Password Test : Make sure your password is at least 8 letters')
+        self.assertEqual(pwd.check_complexity(self.password5), (True, 'Password suceess'), 'Password suceess')
+        self.assertEqual(pwd.check_complexity(self.password6), (False, 'Make sure your password is at max 16 letters'),  'Failed Password Test : Make sure your password is at max 16 letters')
+
+    
+ 
+
+
+   
+if __name__ == '__main__':
+    unittest.main()
